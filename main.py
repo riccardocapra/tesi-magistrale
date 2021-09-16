f3ef5ca554a179c3ce2fc879962e8a80034d2ad5
riccardocapra0@gmail.com <riccardocapra0@gmail.com>
2021-09-16 15:40:57 +0200

    found cam2/velo calib matrix

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from regNet import RegNet
-from utils import data_formatter
+from utils import data_formatter, get_calib
 import torch
 
 device = torch.device("cuda")
 # Specify the dataset to load
 basedir = '/media/RAIDONE/DATASETS/KITTI/ODOMETRY/'
+get_calib(basedir)
 imageTensor, lidar_tensor=data_formatter(basedir)
 model = RegNet()
 model.train()
 imageTensor2 = imageTensor[:,:1,:,:]
-transl, rot = model(imageTensor, lidar_tensor)
+#transl, rot = model(imageTensor, lidar_tensor)
--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def data_formatter(basedir):
     dataset = pykitti.odometry(basedir, sequence)
     depth = dataset.get_velo(0)
     # depth = torch.from_numpy(depth / (2 ** 16)).float()
-    # Le camere 2 e 3 sono quelle a colori, verificato.
+    # Le camere 2 e 3 sono quelle a colori, verificato. Mi prendo la 2.
     rgb_files = dataset.cam2_files
     # print(rgb_files)
     to_tensor = transforms.ToTensor()
@@ -27,9 +27,9 @@ def data_formatter(basedir):
 
 
 def get_calib(basedir):
-    dataset = pykitti.odometry(basedir, 00)
-    dataset.calib
-
+    sequence = '00'
+    dataset = pykitti.odometry(basedir, sequence)
+    print(dataset.calib.T_cam2_velo)
     print("Cam left color:")
     f = open(basedir + "sequences/00/calib.txt", "r")
     lines = f.readlines()
@@ -50,7 +50,7 @@ def get_calib(basedir):
     f.close
 
     print("\n Cam right color:")
-    f = open(basedir + "sequences/00/calib.txt", "r")
+    f = open(basedir + "sequences/"+sequence+"/calib.txt", "r")
     lines = f.readlines()
     print(lines[4])
     cam03Param = lines[4].split()
